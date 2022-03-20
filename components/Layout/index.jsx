33495012212b447c4c95711f431d707051ab5267
allen0524 <william0318563@gmail.com>
2022-03-20 12:54:49 +0800

    【Update】
    1. add loading indicator
    
    2. fix api endpoint

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,12 +1,25 @@
-import Header from "../Header/Header";
+import React from 'react'
+import Header from '../Header/Header'
+import LoadingIndicator from '../LoadingIndicator'
+import Router from 'next/router'
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
+  const [iseFetching, setIsFetching] = React.useState(false)
+  Router.events.on('routeChangeStart', url => {
+    setIsFetching(true)
+  })
+  Router.events.on('routeChangeComplete', url => {
+    setIsFetching(false)
+  })
+
   return (
-    <>
+    <div>
       <Header />
+      ``
       {children}
-    </>
-  );
-};
+      {iseFetching ? <LoadingIndicator /> : null}
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
