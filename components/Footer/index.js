e5f90695085130caa715c6f61c8a3e3a8f92a60e
allen0524 <william0318563@gmail.com>
2022-03-20 13:12:22 +0800

    fix misspelledㄡ

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer className="flex justify-center items-center w-full h-20 bg-pri">
       <div className="text-gray-300">
-        THE F2E 3rd Week 01 Desin by : Jessen Lin, Code by : Allen Li
+        THE F2E 3rd Week 01 Designed by : Jessen Lin, Code by : Allen Li
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
